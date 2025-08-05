42de6502ab1f8f31b8fde8042f4ea06f77e90310
Chester Don Valencerina <vale0192@algonquinlive.com>
2025-08-04 20:51:12 -0400

    Refactor the File Structure

--- src/main/java/viewlayer/ShowMaintenanceByIdServlet.java
+++ src/main/java/viewlayer/ShowMaintenanceByIdServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpSession;
 
 /**
  * Shows maintenance task details based on ID
- * Accessible by all authenticated users
  */
 public class ShowMaintenanceByIdServlet extends HttpServlet {
     private MaintenanceLogLogic logic = new MaintenanceLogLogic();
